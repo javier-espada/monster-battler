b37d5df485394c1576d7992aa1089eca440c8ddd
javier-espada <javier.espada@qaracter.com>
2025-12-22 15:26:29 +0100

    MapMenu main functionality

--- src/main/java/menus/MapMenu.java
+++ src/main/java/menus/MapMenu.java
@@ -1,5 +1,17 @@
-package menus;
+package menus
 
 public class MapMenu {
-
+    Map map = new Map();
+    int option;
+    run(){
+        while(true) {
+            System.out.println("Welcome to lvl" + map.getLevel());
+            System.out.println("What do you wanna do:");
+            System.out.println("1. Explore the next encounter:");
+            System.out.println("2. Face the boss:");
+            Scanner sc = new Scanner();
+            option = sc.nextInt();
+            sc.nextLine();
+        }
+    }
 }
